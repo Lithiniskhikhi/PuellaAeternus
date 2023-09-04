f968f2561180803f6c717e3980d22fde23fe62b1
Lithiniskhikhi <lithiniskhikhi9@gmail.com>
2023-09-04 11:21:58 +0200

    stuff

--- frontend/src/views/SingleProductView.vue
+++ frontend/src/views/SingleProductView.vue
@@ -6,13 +6,12 @@
             <body>
                 <div class="container" v-if="product">
                   <div class="left-side">
-                    <img :src="product.prodUrl" class="left-side" alt=""  />
+                    <img :src="product.prodUrl" class="left-side" alt="" style="height: 28rem; width:18rem" />
                   </div>
                   <div class="right-side">
                     <p id="perfume">PAINTINGS</p>
                     <h1 id="header">{{ product.prodName }}</h1>
-                    <p id="description">A floral, solar and voluptuous interpretation composed by
-                                        Olivier Polge, Perfumer-Creator for the House of CHANEL</p>
+                    <p id="description">A poetic expression, where the canvas breathes with its own life.Layers of texture create a tactile feast for the eyes, inviting closer inspection</p>
                     <div class="price">
                       <h1 id="number-discount">R{{ product.amount }}</h1>
                       <p id="number-full">$169.99</p>
@@ -37,6 +36,7 @@
   export default {
   computed: {
     product() {
+
       return this.$store.state.product;
     },
   },
